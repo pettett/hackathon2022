962b227f5657dc000b6887512ddbd18b9a912973
Maxwell Pettett <43613592+StolenCheese@users.noreply.github.com>
2022-01-22 18:04:50 +0000

    nelson mandella's life story

--- text-factchecker/wikipedia_search.py
+++ text-factchecker/wikipedia_search.py
@@ -14,10 +14,12 @@ import wikipedia
 from wikidata.client import Client
 from nltk.stem.wordnet import WordNetLemmatizer
 import pprint
+import pandas as pd
 
 S = requests.Session()
 
 URL = "https://en.wikipedia.org/w/api.php"
+WIKIDATA = "https://www.wikidata.org/w/api.php"
 client = Client()
 
 pp = pprint.PrettyPrinter(indent=1)
@@ -40,8 +42,52 @@ def get_wikidata_id_for_title(title: str):
     return R.text.split('wikibase_item":"')[1].split('"')[0]
 
 
+def get_wikidata_properties_for_id(id: str):
+    PARAMS = {
+        "format": "json",
+        "action": "wbgetclaims",
+        "entity": id
+    }
+
+    R = S.get(url=WIKIDATA, params=PARAMS)
+
+    d = R.json()["claims"]
+
+    pp.pprint(d["P570"])
+
+    ps = list(d.keys())
+
+    chunksize = 30
+
+    properties = pd.DataFrame(index=d.keys(), columns=["value", "type", "label"])
+
+    properties["type"] = properties.index.map(lambda x: d[x][0]["mainsnak"]["datatype"])
+    properties["value"] = properties.index.map(lambda x: d[x][0]["mainsnak"]["datavalue"]["value"])
+
+    for i in range(0, len(ps), chunksize):
+        # request properties in chunks of 30
+        props = "|".join(ps[i:min(i+chunksize, len(ps))])
+
+        PARAMS = {
+            "format": "json",
+            "action": "wbgetentities",
+            "languages": "en",
+            "ids": props,
+            "props": "labels",
+        }
+
+        PROPS = S.get(url=WIKIDATA, params=PARAMS).json()["entities"]
+
+        for (k, v) in PROPS.items():
+            properties["label"][k] = v["labels"]["en"]["value"]
+
+    return properties
+
+
 wikidata_conversions = {
     "die": "death"
+
+
 }
 
 
@@ -56,7 +102,7 @@ def SearchPhrase(phrase):
     # Get summary infmation
     summary = wikipedia.summary(r, sentences=3)
 
-    #page = wikipedia.page(r)
+    # page = wikipedia.page(r)
 
     # print(page.__dict__)
 
@@ -64,28 +110,28 @@ def SearchPhrase(phrase):
 
     id = get_wikidata_id_for_title(r)
 
+    print(get_wikidata_properties_for_id(id))
+
     data = client.get(id, load=True)
 
-    words = [WordNetLemmatizer().lemmatize(word, 'v') for word in phrase.split(" ")]
+    words = phrase.split(" ")
 
-    words = [wikidata_conversions.get(w, w) for w in words]
+    # words = [WordNetLemmatizer().lemmatize(word, 'v') for word in words]
 
-    # print(words)
+    # words = [wikidata_conversions.get(w, w) for w in words]
+
+    print(words)
 
     for prop in data:
 
-        label = '{!r}'.format(prop.label).split(" ")
+        label = '{!r}'.format(prop.label)[2:-1].split(" ")
 
         for word in words:
             if word in label:
                 print(f"Relevant?: {prop.label}, {data.getlist(prop)}")
-                break
-
-        else:
-            print(f"nothing for {label}")
 
     return summary
 
 
 if __name__ == "__main__":
-    SearchPhrase("nelson mandella died")
+    SearchPhrase("nelson mandella death")
