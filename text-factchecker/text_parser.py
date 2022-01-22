c5144fa408b4a3873c78e860ed915e0d70f1c9cd
Maxwell Pettett <43613592+StolenCheese@users.noreply.github.com>
2022-01-22 16:18:00 +0000

    wikidata from sentance

--- text-factchecker/text_parser.py
+++ text-factchecker/text_parser.py
@@ -1,11 +1,20 @@
 
 from rake_nltk import Rake
 import nltk
+from wikipedia_search import SearchPhrase
 
-incorrect = """
-Nelson Mandella died in 2006, when he was hit by a bus. 
-This caused the current US president, barak obama, to hold a day of mourning
-"""
+
+# Reguires:
+
+# nltk.download('omw-1.4')
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+
+
+incorrect = [
+    "Nelson Mandella died in 2006, when he was hit by a bus",
+    "This caused the current US president, barak obama, to hold a day of mourning"
+]
 
 
 # Uses stopwords for english from NLTK, and all puntuation characters by
@@ -13,10 +22,10 @@ This caused the current US president, barak obama, to hold a day of mourning
 r = Rake()
 
 # Extraction given the text.
-r.extract_keywords_from_text(incorrect)
+# r.extract_keywords_from_text(incorrect)
 
 # Extraction given the list of strings where each string is a sentence.
-# r.extract_keywords_from_sentences( < list of sentences > )
+r.extract_keywords_from_sentences(incorrect)
 
 # To get keyword phrases ranked highest to lowest.
 r.get_ranked_phrases()
@@ -25,6 +34,13 @@ r.get_ranked_phrases()
 
 cutoff = 2
 
+
 for (score, phrase) in r.get_ranked_phrases_with_scores():
     if score < cutoff:
         continue
+
+    data = SearchPhrase(phrase)
+
+    print(f"""||| {phrase} |||:,
+    
+     {data }""")
--- text-factchecker/wikipedia_search.py
+++ text-factchecker/wikipedia_search.py
@@ -8,28 +8,84 @@
 
     MIT License
 """
-
+import re
 import requests
+import wikipedia
+from wikidata.client import Client
+from nltk.stem.wordnet import WordNetLemmatizer
+import pprint
 
 S = requests.Session()
 
 URL = "https://en.wikipedia.org/w/api.php"
+client = Client()
 
-SEARCHPAGE = "Nelson Mandela"
+pp = pprint.PrettyPrinter(indent=1)
 
-PARAMS = {
-    "action": "query",
-    "format": "json",
-    "list": "search",
-    "srsearch": SEARCHPAGE
+
+def trim_html(text: str):
+    return re.sub("<.*?>", "", text)
+
+
+def get_wikidata_id_for_title(title: str):
+    PARAMS = {
+        "action": "query",
+        "format": "json",
+        "prop": "pageprops",
+        "titles": title,
+    }
+
+    R = S.get(url=URL, params=PARAMS)
+
+    return R.text.split('wikibase_item":"')[1].split('"')[0]
+
+
+wikidata_conversions = {
+    "die": "death"
 }
 
-R = S.get(url=URL, params=PARAMS)
-DATA = R.json()
 
-results = DATA['query']['search']
+def SearchPhrase(phrase):
 
-print(results)
+    # SEARCHPAGE = phrase
 
-if results[0]['title'] == SEARCHPAGE:
-    print("Your search page '" + SEARCHPAGE + "' exists on English Wikipedia")
+    results = wikipedia.search(phrase, results=1)
+
+    r = results[0]
+
+    # Get summary infmation
+    summary = wikipedia.summary(r, sentences=3)
+
+    #page = wikipedia.page(r)
+
+    # print(page.__dict__)
+
+    # Get isolated facts about the topic
+
+    id = get_wikidata_id_for_title(r)
+
+    data = client.get(id, load=True)
+
+    words = [WordNetLemmatizer().lemmatize(word, 'v') for word in phrase.split(" ")]
+
+    words = [wikidata_conversions.get(w, w) for w in words]
+
+    # print(words)
+
+    for prop in data:
+
+        label = '{!r}'.format(prop.label).split(" ")
+
+        for word in words:
+            if word in label:
+                print(f"Relevant?: {prop.label}, {data.getlist(prop)}")
+                break
+
+        else:
+            print(f"nothing for {label}")
+
+    return summary
+
+
+if __name__ == "__main__":
+    SearchPhrase("nelson mandella died")
